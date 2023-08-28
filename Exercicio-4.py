61079ed6a37edd69b8366b4ab840848e1312ba1a
GEOVANAAPROGRAMMER <rmm99646@fiap.com.br>
2023-08-28 19:50:00 -0300

    Função que retorna o resultado do cálculo da variância populacional.

--- Exercicio-4.py
+++ Exercicio-4.py
@@ -10,7 +10,24 @@ def calcular_variancia_amostral(dados):
     variancia = sum(diferenca_quad) / (len(dados) - 1)
     return variancia
 
-dados = [39, 38, 27, 22, 20, 17, 10, 10, 10, 10, 7, 7, 7, 7, 6]
-variancia_amostral = calcular_variancia_amostral(dados)
+va = [39, 38, 27, 22, 20, 17, 10, 10, 10, 10, 7, 7, 7, 7, 6]
+variancia_amostral = calcular_variancia_amostral(va)
 print("O resultado da variancia amostral desses valores é :   {:.2f}".format(
     variancia_amostral))
+
+
+def calcular_media(dados):
+    soma = sum(dados)
+    media = soma / len(dados)
+    return media
+
+def calcular_variancia_populacional(dados):
+    media = calcular_media(dados)
+    diferenca_quad = [(x - media) ** 2 for x in dados]
+    variancia = sum(diferenca_quad) / len(dados)
+    return variancia
+
+vp = [39, 38, 27, 22, 20, 17, 10, 10, 10, 10, 7, 7, 7, 7, 6]
+variancia_populacional = calcular_variancia_populacional(vp)
+print("O resultado da variancia populacional desses valores é :   {:.2f}".format(
+    variancia_populacional))
