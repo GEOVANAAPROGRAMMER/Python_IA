15fe4330bc7ea3c96ea38d49f7f6cf920b3fee99
GEOVANAAPROGRAMMER <rmm99646@fiap.com.br>
2023-08-28 19:53:10 -0300

    Cálculo da função de desvio padrão amostral.

--- Exercicio-4.py
+++ Exercicio-4.py
@@ -31,3 +31,21 @@ vp = [39, 38, 27, 22, 20, 17, 10, 10, 10, 10, 7, 7, 7, 7, 6]
 variancia_populacional = calcular_variancia_populacional(vp)
 print("O resultado da variancia populacional desses valores é :   {:.2f}".format(
     variancia_populacional))
+
+
+def calcular_media(dados):
+    soma = sum(dados)
+    media = soma / len(dados)
+    return media
+
+
+def calcular_desvio_padrao_amostral(dados):
+    media = calcular_media(dados)
+    diferenca_quad = [(x - media) ** 2 for x in dados]
+    variancia = sum(diferenca_quad) / (len(dados) - 1)
+    desvio_padrao = variancia ** 0.5
+    return desvio_padrao
+
+dvpa = [39, 38, 27, 22, 20, 17, 10, 10, 10, 10, 7, 7, 7, 7, 6]
+desvio_padrao_amostral = calcular_desvio_padrao_amostral(dvpa)
+print("Desvio padrão amostral:", desvio_padrao_amostral)
