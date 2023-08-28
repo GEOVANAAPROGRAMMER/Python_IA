f2f74c0843210e3e78f882e086ff4e89cf8860ca
GEOVANAAPROGRAMMER <rmm99646@fiap.com.br>
2023-08-28 20:05:44 -0300

    Função de cálculo de incerteza média.

--- Exercicio-4.py
+++ Exercicio-4.py
@@ -16,11 +16,6 @@ print("O resultado da variancia amostral desses valores é :   {:.2f}".format(
     variancia_amostral))
 
 
-def calcular_media(dados):
-    soma = sum(dados)
-    media = soma / len(dados)
-    return media
-
 def calcular_variancia_populacional(dados):
     media = calcular_media(dados)
     diferenca_quad = [(x - media) ** 2 for x in dados]
@@ -33,12 +28,6 @@ print("O resultado da variancia populacional desses valores é :   {:.2f}".forma
     variancia_populacional))
 
 
-def calcular_media(dados):
-    soma = sum(dados)
-    media = soma / len(dados)
-    return media
-
-
 def calcular_desvio_padrao_amostral(dados):
     media = calcular_media(dados)
     diferenca_quad = [(x - media) ** 2 for x in dados]
@@ -48,4 +37,29 @@ def calcular_desvio_padrao_amostral(dados):
 
 dvpa = [39, 38, 27, 22, 20, 17, 10, 10, 10, 10, 7, 7, 7, 7, 6]
 desvio_padrao_amostral = calcular_desvio_padrao_amostral(dvpa)
-print("Desvio padrão amostral:", desvio_padrao_amostral)
+print("O resultado da desvio padrão amostral desses valores é :   {:.2f}".format(
+    desvio_padrao_amostral))
+
+
+def calcular_desvio_padrao_populacional(dados):
+    media = calcular_media(dados)
+    diferenca_quad = [(x - media) ** 2 for x in dados]
+    variancia = sum(diferenca_quad) / len(dados)
+    desvio_padrao = variancia ** 0.5
+    return desvio_padrao
+
+desvio_p = [39, 38, 27, 22, 20, 17, 10, 10, 10, 10, 7, 7, 7, 7, 6]
+desvio_padrao_populacional = calcular_desvio_padrao_populacional(desvio_p)
+print("O resultado da desvio padrão populacional desses valores é :   {:.2f}".format(
+    desvio_padrao_populacional))
+
+
+def calcular_incerteza_media(dados):
+    desvio_padrao_amostral = calcular_desvio_padrao_amostral(dados)
+    incerteza_media = desvio_padrao_amostral / (len(dados) ** 0.5)
+    return incerteza_media
+
+incert_med = [39, 38, 27, 22, 20, 17, 10, 10, 10, 10, 7, 7, 7, 7, 6]
+incerteza_media = calcular_incerteza_media(incert_med)
+print("O resultado da incerteza da média desses valores é :   {:.2f}".format(
+    incerteza_media))
